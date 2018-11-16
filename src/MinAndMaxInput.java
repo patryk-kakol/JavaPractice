4cda3266429855649104c86217e955e8364f3ff6
Patryk Kąkol <patryklkakol@gmail.com>
2018-11-16 01:57:45 +0100

    last part of methods practice

--- src/MinAndMaxInput.java
+++ src/MinAndMaxInput.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MinAndMaxInput {
+    public static void main(String[] args) {
+
+        int max = 0;
+        int min = 0;
+        Scanner reader = new Scanner(System.in);
+
+        while ( true ) {
+            System.out.print("Enter number: ");
+
+            if (!reader.hasNextInt()) {
+                break;
+            } else {
+                if (reader.nextInt() > max) max = reader.nextInt();
+                if (reader.nextInt() < min) min = reader.nextInt();
+
+            }
+            //reader.nextLine();
+        }
+        reader.close();
+        System.out.println("Max = " + max + " || Min = " + min);
+    }
 }
--- src/ReadingInput.java
+++ src/ReadingInput.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ReadingInput {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        Scanner reader = new Scanner(System.in);
+
+        while ( i <= 10 ) {
+            System.out.println("Enter number #" + i + ":");
+
+            if (!reader.hasNextInt()) {
+                System.out.println("Invalid Number");
+            } else {
+                sum += reader.nextInt();
+                i += 1;
+            }
+            reader.nextLine();
+        }
+        reader.close();
+        System.out.println("Sum = " + sum);
+    }
 }
